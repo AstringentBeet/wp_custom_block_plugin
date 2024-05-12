cde13cd12dbb7eb048f204f7487e1ccd1723c12a
AstringentBeet <juxtaposedowl@gmail.com>
2024-05-12 03:32:38 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        Still figuring that out.
+ * Description:       A plugin for adding blocks to a theme
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Some Rando
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+ if(!function_exists('add_action')) {
+    echo "Seems like you found yourself in a place you shouldn't be. Nice try, though.";
+    exit;
+ }
