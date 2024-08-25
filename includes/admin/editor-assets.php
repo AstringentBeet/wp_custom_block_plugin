ff728b34edcb9c2a559651de062d11fc10640696
AstringentBeet <juxtaposedowl@gmail.com>
2024-08-25 10:46:28 -0500

    I spoke too sun about the global inline styles warning...finished the format tool bar, anyways.

--- includes/admin/editor-assets.php
+++ includes/admin/editor-assets.php
@@ -1,17 +1,11 @@
 <?php
 
 function up_enqueue_block_assets(){
-    $editor_asset = include(UP_PLUGIN_DIR . 'build/block-editor/index.asset.php');
    
     wp_enqueue_script(
         'up_editor',
-        plugins_url('/build/block-editor/index.js', UP_PLUGIN_FILE),
-        $editor_asset['dependencies'],
-        $editor_asset['version'],
-        true
     );
     wp_enqueue_style(
         'up_editor',
-        plugins_url('/build/block-editor/index.css', UP_PLUGIN_FILE),
     );
 }
