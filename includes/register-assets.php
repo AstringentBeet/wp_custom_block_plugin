b2a7de9c321f7f0fb21832bf03df2c29e862cc8f
AstringentBeet <juxtaposedowl@gmail.com>
2024-08-25 03:51:37 -0500

    I notice I don't get a global-inline-styles error with the styles and scripts enqueued this way...

--- includes/register-assets.php
+++ includes/register-assets.php
@@ -16,18 +16,5 @@ function up_register_assets() {
         $adminAssets['version'],
         true
     );
-
-    $editor_asset = include(UP_PLUGIN_DIR . 'build/block-editor/index.asset.php');
-   
-    wp_register_script(
-        'up_editor',
-        plugins_url('/build/block-editor/index.js', UP_PLUGIN_FILE),
-        $editor_asset['dependencies'],
-        $editor_asset['version'],
-        true
-    );
-    wp_register_style(
-        'up_editor',
-        plugins_url('/build/block-editor/index.css', UP_PLUGIN_FILE),
-    );
+    
 }
